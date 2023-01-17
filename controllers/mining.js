5bcdbd4767bf4bd85a164c6ba5440fdaf638a6e6
Ibrahim abdulraheem <ibroraheem95@gmail.com>
2023-01-17 09:45:41 +0100

    Update mining.js

--- controllers/mining.js
+++ controllers/mining.js
@@ -21,7 +21,7 @@ const stopMining = async (req, res) => {
     try {
         const token = req.headers.authorization.split(' ')[1]
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findOne(decoded.id)
+        const user = await User.findById(decoded.id)
         if (!user) return res.status(401).send({ message: "User not found!" })
          if(user.balance.isMining){
         user.balance.isMining = false
