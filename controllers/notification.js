4babba9218f6796a1fee5f526ad10d1dd165eeb0
ibnyahyah <yahyahridwan665@gmail.com>
2023-01-25 23:43:12 +0100

    update

--- controllers/notification.js
+++ controllers/notification.js
@@ -81,7 +81,7 @@ const getFeedBacks = async (req, res) => {
     try {
         const token = req.headers.authorization.split(' ')[1]
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const admin = await Admin.findOne({ _id: decoded.id })
+        const admin = await Admin.findById(decoded.id)
         if (!admin) return res.status(401).json({ message: "Unauthorized" })
         const feedbacks = await FeedBack.find({})
         res.status(200).json(feedbacks)
