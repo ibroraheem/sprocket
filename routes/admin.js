a84c1b219843e1353368baf0f10a1fb87a88e752
ibnyahyah <yahyahridwan665@gmail.com>
2023-01-26 07:15:23 +0100

    adding version

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login, getUsers, getUser, getAnalytics } = require('../controllers/admin');
+const { register, login, getUsers, getUser, getAnalytics, newVersion, getVersion } = require('../controllers/admin');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/users', getUsers);
 router.get('/user/:id', getUser);
 router.get('/analytics', getAnalytics);
+router.post('/new-version', newVersion);
+router.post('/versions', getVersion);
 
 
 module.exports = router;
