9b18a1ef10f60a29540720b4dcd5216969da7b39
Ibrahim abdulraheem <ibroraheem95@gmail.com>
2023-01-29 14:15:56 +0100

    Update mining.js

--- controllers/mining.js
+++ controllers/mining.js
@@ -6,8 +6,19 @@ const mine = async (req, res) => {
         const token = req.headers.authorization.split(' ')[1]
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findById(decoded.id)
-        if (!user) return res.status(401).send({ message: "User not found!" })
-        if (!user.balance.isMining) {
+        if (!user) return res.status(401).send({ message: "User not found!" });
+         // date comparison
+        const date = new Date(user.balance.miningTime).getFullYear() + '-' + new Date(user.balance.miningTime).getMonth() + '-' + new Date(user.balance.miningTime).getDate();
+        const today = new Date().getFullYear() + '-' + new Date().getMonth() + '-' + new Date().getDate();
+        if (user.balance.isMining && date !== today) {
+            //Here I am adding the refferal earn also to the user, so is earning is determine base on his friends
+            // referral length / 100 * 0.2 = team rate
+            let referralsEarnCount = user.referrals.length / 100 * 0.2;
+            let minedReward = 240 + referralsEarnCount;
+            user.balance.isMining = false
+            user.balance.minedBalance += minedReward;
+            await user.save()
+        }else if (!user.balance.isMining) {
             user.balance.isMining = true;
             user.balance.miningTime = Date.now();
             await user.save();
